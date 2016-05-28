91f818aa2058a6a6306ca21e9dd1f4139156d6ee
Elviss Kuštans <n3o59hf@gmail.com>
2016-05-28 03:09:46 +0300

    Make default URLs visible from module

--- miracl_api_tornado/__init__.py
+++ miracl_api_tornado/__init__.py
@@ -2,3 +2,4 @@ from auth import MiraclMixin, MiraclAuthRequestHandler
 from auth import is_authenticated, logout, get_login_url
 from auth import get_email, get_user_id, refresh_user_data
 from auth import MIRACL_COOKIE_TOKEN_KEY, OAUTH_SETTINGS_KEY
+from auth import OAUTH_ACCESS_TOKEN_URL, OAUTH_AUTHORIZE_URL, OAUTH_USERINFO_URL
